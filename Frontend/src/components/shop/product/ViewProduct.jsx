3f31772ceffc4fc8f5a023311928cc78a4ea22da
smhasanjamil <smhasanjamil23@gmail.com>
2024-09-27 19:45:03 +0600

    View individual product by id

--- Frontend/src/components/shop/product/ViewProduct.jsx
+++ Frontend/src/components/shop/product/ViewProduct.jsx
@@ -1,13 +1,220 @@
 import { useLoaderData } from "react-router-dom";
 
 const ViewProduct = () => {
-    const product = useLoaderData();
-    console.log(product);
-    return (
-        <div>
-            View product
+  const product = useLoaderData();
+  console.log(product);
+  return (
+    <>
+      <div className="max-w-7xl mx-auto px-4 lg:px-0">
+        <div className="p-4 lg:max-w-5xl max-w-lg mx-auto">
+          <div className="grid items-start grid-cols-1 lg:grid-cols-2 gap-6 max-lg:gap-12">
+            <div className="w-full lg:sticky top-0">
+              <img
+                src={product?.image}
+                alt="Product"
+                className="w-full rounded-md object-cover"
+              />
+            </div>
+
+            <div>
+              <h2 className="text-2xl font-bold text-gray-800">
+                {product?.name}
+              </h2>
+              <div className="mt-4">
+                <p className="text-gray-800 text-xl font-bold">
+                  {product?.price} &#2547;
+                </p>
+              </div>
+
+              <div className="flex space-x-2 mt-4">
+                <svg
+                  className="w-5 fill-green-600"
+                  viewBox="0 0 14 13"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                </svg>
+                <svg
+                  className="w-5 fill-green-600"
+                  viewBox="0 0 14 13"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                </svg>
+                <svg
+                  className="w-5 fill-green-600"
+                  viewBox="0 0 14 13"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                </svg>
+                <svg
+                  className="w-5 fill-green-600"
+                  viewBox="0 0 14 13"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                </svg>
+                <svg
+                  className="w-5 fill-[#CED5D8]"
+                  viewBox="0 0 14 13"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                </svg>
+              </div>
+
+              <div className="mt-8">
+                <h3 className="text-xl font-bold text-gray-800">Items</h3>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="flex items-center px-2.5 py-1.5 border border-gray-300 text-gray-800 text-xs outline-none bg-transparent rounded-md"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-2.5 fill-current"
+                      viewBox="0 0 124 124"
+                    >
+                      <path
+                        d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"
+                        data-original="#000000"
+                      ></path>
+                    </svg>
+
+                    <span className="mx-2.5">1</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-2.5 fill-current"
+                      viewBox="0 0 42 42"
+                    >
+                      <path
+                        d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"
+                        data-original="#000000"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <button
+                type="button"
+                className="w-full mt-8 px-6 py-3 bg-green-600 hover:bg-green-700 text-white text-sm font-semibold rounded-md"
+              >
+                Add to cart
+              </button>
+
+              <div className="mt-8">
+                <h3 className="text-xl font-bold text-gray-800">Description</h3>
+                <p className="space-y-3 list-disc mt-4 text-sm text-gray-800 text-justify">
+                  {product?.description}
+                </p>
+              </div>
+
+              <div className="mt-8">
+                <h3 className="text-xl font-bold text-gray-800">
+                  Reviews({product?.review})
+                </h3>
+                <div className="space-y-3 mt-4">
+                  <div className="flex items-center">
+                    <p className="text-sm text-gray-800 font-bold">5.0</p>
+                    <svg
+                      className="w-5 fill-green-600 ml-1.5"
+                      viewBox="0 0 14 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                    </svg>
+                    <div className="bg-gray-300 rounded-md w-full h-2 ml-3">
+                      <div className="w-2/3 h-full rounded-md bg-green-600"></div>
+                    </div>
+                    <p className="text-sm text-gray-800 font-bold ml-3">66%</p>
+                  </div>
+
+                  <div className="flex items-center">
+                    <p className="text-sm text-gray-800 font-bold">4.0</p>
+                    <svg
+                      className="w-5 fill-green-600 ml-1.5"
+                      viewBox="0 0 14 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                    </svg>
+                    <div className="bg-gray-300 rounded-md w-full h-2 ml-3">
+                      <div className="w-1/3 h-full rounded-md bg-green-600"></div>
+                    </div>
+                    <p className="text-sm text-gray-800 font-bold ml-3">33%</p>
+                  </div>
+
+                  <div className="flex items-center">
+                    <p className="text-sm text-gray-800 font-bold">3.0</p>
+                    <svg
+                      className="w-5 fill-green-600 ml-1.5"
+                      viewBox="0 0 14 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                    </svg>
+                    <div className="bg-gray-300 rounded-md w-full h-2 ml-3">
+                      <div className="w-1/6 h-full rounded-md bg-green-600"></div>
+                    </div>
+                    <p className="text-sm text-gray-800 font-bold ml-3">16%</p>
+                  </div>
+
+                  <div className="flex items-center">
+                    <p className="text-sm text-gray-800 font-bold">2.0</p>
+                    <svg
+                      className="w-5 fill-green-600 ml-1.5"
+                      viewBox="0 0 14 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                    </svg>
+                    <div className="bg-gray-300 rounded-md w-full h-2 ml-3">
+                      <div className="w-1/12 h-full rounded-md bg-green-600"></div>
+                    </div>
+                    <p className="text-sm text-gray-800 font-bold ml-3">8%</p>
+                  </div>
+
+                  <div className="flex items-center">
+                    <p className="text-sm text-gray-800 font-bold">1.0</p>
+                    <svg
+                      className="w-5 fill-green-600 ml-1.5"
+                      viewBox="0 0 14 13"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+                    </svg>
+                    <div className="bg-gray-300 rounded-md w-full h-2 ml-3">
+                      <div className="w-[6%] h-full rounded-md bg-green-600"></div>
+                    </div>
+                    <p className="text-sm text-gray-800 font-bold ml-3">6%</p>
+                  </div>
+                </div>
+
+                <button
+                  type="button"
+                  className="w-full mt-8 px-6 py-2.5 border border-green-600 bg-transparent text-gray-800 text-sm font-semibold rounded-md"
+                >
+                  Read all reviews
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default ViewProduct;
